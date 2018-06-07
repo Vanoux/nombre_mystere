1724eb867ff4f020679ede9eadbd7ea028c81f3e
Vanoux <vanessa.roux891@orange.fr>
2018-06-07 15:26:10 +0200

    ca marche, youpi!

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 //Declaration des dépendances 
 var express = require('express');
+var bodyParser = require("body-parser");
+var lodash = require("lodash");
 
+
+var nombre;
+var message = "";
+var aleatoire = lodash.random(9);
 
 
 //Création d'un objet app en appelant la fonction express().
 var app = express();
-
-
+    app.use(bodyParser.urlencoded ({ extended: true }));
 
 // Appel de la route(url) à laquelle l'application doit répondre => fonction callback pour appeler la page ejs
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {// req = request et res = response
     res.render('action.ejs', {nombre, message});
+    //res.render => méthode qui génère un modèle du vue
 });
 
 
-// Appel l'action "url" quand click sur ok 
-// app.post ou.get ? ('/url', function(req, res) {
+
+// récupère le chiffre quand click sur ok
+app.post('/url', function(req, res) {
+    nombre = req.body.nombre;
+    console.log(nombre);
+   // res.render('action.ejs', {nombre, message}) 
     
-//}) COMMENT FAIRE ENTRER LE NUM DE L'UTILISATEUR??????????
-
-
-
+    
+    if (nombre == aleatoire){
+        message = 'Gagné ! Le nombre mystère était : ' + aleatoire
+        console.log('Gagné ! Le nombre mystère était : ' + aleatoire);
+    }
+    else if (nombre > aleatoire){
+        message = 'C\'est moins ! Retente'
+        console.log('C\'est moins ! Retente');
+    }
+    else if (nombre < aleatoire){
+        message = 'C\'est plus ! Retente'
+        console.log('C\'est plus ! Retente');
+    }
+   
+    res.render('action.ejs', {
+       message:message,
+       nombre:nombre
+    })
+});
 
 
 
